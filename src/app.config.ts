a2972902a1951eb50dbbf9e8553f3a849b3dc866
luosmallrui <luoxiaorui115815@qq.com>
2026-01-24 17:41:19 +0800

    upload

--- src/app.config.ts
+++ src/app.config.ts
@@ -49,6 +49,7 @@ export default defineAppConfig({
     ],
   },
   requiredPrivateInfos: ['getLocation'],
+  lazyCodeLoading: 'requiredComponents',
   permission: {
     'scope.userLocation': {
       desc: '你的位置信息将用于小程序位置接口的效果展示',
